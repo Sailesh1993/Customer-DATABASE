545ec131c9b5fa7f90a447bbe1e1dfaffe1846ad
Sailesh1993 <karki.saileshnep@gmail.com>
2023-06-11 23:09:28 +0300

    modify the customer database deleting commented code

--- src/Program.cs
+++ src/Program.cs
@@ -2,53 +2,6 @@
 {
     static void Main( string[] args)
     {
-       /* CustomerDatabase customerDatabase = new CustomerDatabase();
-
-        // Test the functionality
-        Customer customer1 = new Customer
-        {
-            Id = 1,
-            FirstName = "John",
-            LastName = "Doe",
-            Email = "john.doe@example.com",
-            Address = "123 Main St"
-        };
-        customerDatabase.AddCustomer(customer1);
-
-        Customer customer2 = new Customer
-        {
-            Id = 2,
-            FirstName = "Jane",
-            LastName = "Smith",
-            Email = "jane.smith@example.com",
-            Address = "456 Elm St"
-        };
-        customerDatabase.AddCustomer(customer2);
-
-        customerDatabase.SearchCustomerById(1);
-
-        Customer updatedCustomer = new Customer
-        {
-            Id = 2,
-            FirstName = "Jane",
-            LastName = "Smith",
-            Email = "jane.smith@example.com",
-            Address = "789 Oak St"
-        };
-        customerDatabase.UpdateCustomer(2, updatedCustomer);
-
-        customerDatabase.SearchCustomerById(2);
-
-        customerDatabase.DeleteCustomer(2);
-
-        customerDatabase.Undo();
-        customerDatabase.SearchCustomerById(2);
-
-        customerDatabase.Redo();
-        customerDatabase.SearchCustomerById(1);
-
-        Console.ReadLine(); */
-    
      // File path for storing customer data
         string filePath = "customers.csv";
 
